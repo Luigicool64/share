ad8d89fab0439c73d0ab96586bcc5367bd5c7506
corentinb <beaujoiscorentin@gmail.com>
2024-10-16 09:37:14 +0200

    fix:régle de mot de passe

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,10 +9,9 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -38,14 +37,9 @@ class RegistrationFormType extends AbstractType
                 'label_attr' => ['class'=> 'fw-bold'],
                 'attr' => ['autocomplete' => 'new-password','class'=> 'form-control',],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/',
+                        'message' => 'Le mot de passe doit contenir 12 caratères au moins une lettre majuscule, une lettre minuscule, un chiffre et un caratére spècial',
                     ]),
                 ],
             ])
